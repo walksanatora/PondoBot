0e3bb345f45dc22db42474344d5683fcad41150f
walksanatora <walkerffo22@hotmail.com>
2021-10-06 22:46:32 -0700

    html email

--- libs/email.js
+++ libs/email.js
@@ -55,14 +55,15 @@ async function authorize(credentials) {
 
 
 function makeBody(to, from, subject, message) {
-    var str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
-        "MIME-Version: 1.0\n",
-        "Content-Transfer-Encoding: 7bit\n",
-        "to: ", to, "\n",
-        "from: ", from, "\n",
-        "subject: ", subject, "\n\n",
+    var str = [
+        'Content-Type: text/html; charset="UTF-8"',
+        "MIME-Version: 1.0",
+        "Content-Transfer-Encoding: 7bit",
+        "to: ", to, "",
+        "from: ", from, "",
+        "subject: ", subject, "\n",
         message
-    ].join('');
+    ].join('\n');
 
     var encodedMail = new Buffer.from(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
         return encodedMail;
