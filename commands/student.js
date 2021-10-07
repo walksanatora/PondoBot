8ac3d02279023b16bfe3714be7019ecc8166fbbc
walksanatora <walkerffo22@hotmail.com>
2021-10-06 21:26:24 -0700

    emailVerified role

--- commands/student.js
+++ commands/student.js
@@ -69,14 +69,20 @@ async function func(interaction,client){
 				await interaction.reply({content:`your are currently in ${noToYear[db.user[user].grade]} year`,ephemeral:true})
 			break;
 		case 'get-role':
-				(db.user[user].grade == null)? await interaction.reply({content:'grade not configured',ephemeral:true}):null
-				guildID = interaction.guild.id
-				if( ! has(guildID,Object.keys(db.server))) {await interaction.reply({content:'Not Setup, ask someone with `manage channels` to set it up',ephemeral:false})} else{
-					interaction.member.roles.remove(db.server[guildID].grade)
-					interaction.member.roles.add(db.server[guildID].grade[db.user[user].grade - 1])
-					await interaction.reply({content:'roles given',ephemeral:true})
+			if ( db.server[guildID].emailRole != undefined){
+				interaction.member.roles.remove(db.server[guildID].emailVerified)
+				if (db.user[user].emailVerified){
+					interaction.member.roles.add(db.server[guildID].emailVerified)	
 				}
-			break;
+			}
+			(db.user[user].grade == null)? await interaction.reply({content:'grade not configured',ephemeral:true}):null
+			guildID = interaction.guild.id
+			if( ! has(guildID,Object.keys(db.server))) {await interaction.reply({content:'Not Setup, ask someone with `manage channels` to set it up',ephemeral:false})} else{
+				interaction.member.roles.remove(db.server[guildID].grade)
+				interaction.member.roles.add(db.server[guildID].grade[db.user[user].grade - 1])
+			}
+			await interaction.reply({content:'roles given',ephemeral:true})
+		break;
 		case 'verify-email':
 				if (interaction.options.getString('key') == null && interaction.options.getString('username') == null ){
 					await interaction.reply({content: 'no option specified',ephemeral:true})
