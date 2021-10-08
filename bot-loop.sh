5fc64767cd099239626a15a2c10ce8638cea358b
walksanatora <walkerffo22@hotmail.com>
2021-10-07 18:08:17 -0700

    ;

--- bot-loop.sh
+++ bot-loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-while true; do
+while true; do;
 	git pull -f 2>&1 | tee -a bot.log
 	npm i 2>&1 | tee -a bot.log
 	node bot.js 2>&1 | tee -a bot.log
 	if test $? != 0; then node fallback.js 2>&1 | tee -a bot.log
 	tail -n100 bot.log | tee bot.log
 	echo 'logs cut down to 100 lines'
-done
+done;
