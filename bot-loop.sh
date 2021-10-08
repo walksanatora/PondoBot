af870df6d80ba688ebc53d6b16652c4e1b6698d1
walksanatora <walkerffo22@hotmail.com>
2021-10-07 18:10:11 -0700

    echo

--- bot-loop.sh
+++ bot-loop.sh
@@ -3,7 +3,10 @@ while true; do
 	git pull -f 2>&1 | tee -a bot.log
 	npm i 2>&1 | tee -a bot.log
 	node bot.js 2>&1 | tee -a bot.log
-	if test $? != 0; then node fallback.js 2>&1 | tee -a bot.log;fi
+	if test $? != 0; then 
+		echo 'running in fallback mode'
+		node fallback.js 2>&1 | tee -a bot.log
+	fi
 	tail -n100 bot.log | tee bot.log
 	echo 'logs cut down to 100 lines'
 done
