f012824fbf55453ef5a5027be1a19834611338c2
walksanatora <walkerffo22@hotmail.com>
2021-10-07 19:21:14 -0700

    stderr to log

--- bot-loop.sh
+++ bot-loop.sh
@@ -2,7 +2,7 @@
 while true; do
 	git pull -f 2>&1 >> bot.log
 	npm i 2>&1 >> bot.log
-	node bot.js 2>&1 >> bot.log
+	node bot.js 2>> bot.log
 	if test $? != 0; then 
 		echo 'running in fallback mode'
 		node fallback.js 2>&1 >> bot.log
