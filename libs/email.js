850dbba978d971829cbc904d94fdcdbc5f49ac13
walksanatora <walkerffo22@hotmail.com>
2021-10-14 20:13:38 -0700

    test email

--- libs/email.js
+++ libs/email.js
@@ -35,15 +35,13 @@ async function authorize(credentials) {
 		console.log('Authorize this app by visiting this url:', authUrl);
         
         const code = prompt('Enter code from that page here:');
-		await oAuth2Client.getToken(code, (err, token) => {
-			if (err) return console.error('Error retrieving access token', err);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile('token.json', JSON.stringify(token), (err) => {
-				if (err) return console.error(err);
-		  	});
-		});
-        return oAuth2Client
+		const token = await oAuth2Client.getToken(code)
+        console.log(token)
+		oAuth2Client.setCredentials(token);
+		fs.writeFile('token.json', JSON.stringify(token), (err) => {
+			if (err) return console.error(err);
+	  	});
+	    return oAuth2Client
 	};
     if (token != null) {
         oAuth2Client.setCredentials(token);
---
+++ testEmail.js
@@ -0,0 +1,19 @@
+const email = require('./libs/email.js')
+const creds = require('./credentials.json')
+async function main(){
+	const OAuth2 = await email.authorize(creds)
+	const message = [
+		`Hello There walksanator`,
+		'You appear to have to ran a email verification, to verify just run',
+		`<b><i>/students verify-email key:INSERT_KEY_HERE</i></b>`,
+		'cant wait to see you :)'
+	].join('<br>\n')
+	console.log(OAuth2)
+	console.log(message)
+	await email.sendMessage(OAuth2,'fowl21043@eduhsd.k12.ca.us','PondoBot verification',message)
+	console.log('email sent')
+}
+
+main().catch(error=>
+	console.log(error.stack)
+)
