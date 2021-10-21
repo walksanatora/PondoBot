5b1ed3b14b11f653a54fa862874587052ba9ccf5
walksanatora <walkerffo22@hotmail.com>
2021-10-20 18:03:06 -0700

    code verification

--- commands/classroom.js
+++ commands/classroom.js
@@ -14,38 +14,41 @@ const data = new SlashCommandBuilder()
 		)
 	)
 
-	async function func(interaction,client){ 
+async function func(interaction,client){ 
 	const db = require('../storage.json')
 	const guildID = interaction.guild.id
 	const userID = interaction.user.id
+	const OAAuth = require('../clsAuth.json')
 	switch (interaction.options.getSubcommand(true)) {
 		case 'link':
 			const code = interaction.options.getString('code')
 			if (code == undefined){
 				if(db.user[userID].auth == undefined){
-					const OAAuth = require('../clsAuth.json')
 					var out = await classroom.authorize(OAAuth)
 					if (typeof out == 'string'){
 						await interaction.reply({content:`Please head [here](${out}) to get code`,ephemeral:true})
-					} else if (typeof out == 'object'){
-						db.user[userID].auth = out[0]
-						await interaction.reply({content:'linked classroom with bot',ephemeral:true})
 					}
 				} else {
 					interaction.reply({content:'allready linked',ephemeral:(db.server[guildID].showMessages)? false:true})
+				}
+			} else {
+				const token = await classroom.authorize(OAAuth,code)
+				if (typeof token == 'object'){
+					db.user[userID].auth = out[0]
+					await interaction.reply({content:'linked classroom with bot',ephemeral:true})
 				}
 			}
 			break;
 		default:
 			await interaction.reply({content:'invalid command',ephemeral:(db.server[guildID].showMessages)? false:true})
-	}
+		}
 	fs.writeFileSync('storage.json',JSON.stringify(db),'utf-8')
 }
 
 module.exports={
 	'data':data,
 	'helpStr':"list off some device status information",
-	'canDeploy':false,
-	'guildIds':[],
+	'canDeploy':true,
+	'guildIds':['783738781097263140'],
 	'function': func
 }
